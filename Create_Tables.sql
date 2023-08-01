cb0399d4373ea40647947e11b7fba7b6f44f3ae6
ppongpeera <125846379+ppongpeera@users.noreply.github.com>
2023-08-02 03:43:10 +0700

    Create_Tables.sql

---
+++ Create_Tables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE "AppleStore" 
+  ( "field1" INTEGER, 
+    "id" INTEGER, 
+    "track_name" TEXT, 
+    "size_bytes" INTEGER, 
+    "currency" TEXT, 
+    "price" INTEGER, 
+    "rating_count_tot" INTEGER, 
+    "rating_count_ver" INTEGER, 
+    "user_rating" INTEGER, 
+    "user_rating_ver" INTEGER, 
+    "ver" TEXT, 
+    "cont_rating" TEXT, 
+    "prime_genre" TEXT, 
+    "sup_devices_num" INTEGER, 
+    "ipadSc_urls_num" INTEGER, 
+    "lang_num" INTEGER, 
+    "vpp_lic" INTEGER )
+
+
+CREATE TABLE "appleStore_description" 
+  ( "id" INTEGER, 
+    "track_name" TEXT, 
+    "size_bytes" INTEGER, 
+    "app_desc" TEXT )
